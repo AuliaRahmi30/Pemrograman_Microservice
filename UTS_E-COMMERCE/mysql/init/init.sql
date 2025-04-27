4259e18ba175db356210dbfac94eaa4f3dc09556
AuliaRahmi30 <167853790+AuliaRahmi30@users.noreply.github.com>
2025-04-27 16:16:53 +0700

    UTS_CASE A

---
+++ UTS_E-COMMERCE/mysql/init/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS dbcomers_item;
+CREATE DATABASE IF NOT EXISTS dbcomers_user;
+CREATE DATABASE IF NOT EXISTS dbcomers_payment;
+-- Tambahkan database lain jika diperlukan
---
+++ UTS_E-COMMERCE/payment-service/src/main/java/com/utscaseA/payment_service/model/Payment.java
@@ -0,0 +1,49 @@
+package com.utscaseA.payment_service.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double amount;
+    private String status;
+
+    // Constructor tanpa parameter
+    public Payment() {}
+
+    // Constructor dengan parameter
+    public Payment(Double amount, String status) {
+        this.amount = amount;
+        this.status = status;
+    }
+
+    // Getters dan Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
